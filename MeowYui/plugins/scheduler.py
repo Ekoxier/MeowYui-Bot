55ab31cec5eb9888b34024932e45712ff9bf7b7a
skyar0423 <skyar0423@gmail.com>
2020-06-14 00:14:01 +0800

    Bug fixed.

--- MeowYui/plugins/scheduler.py
+++ MeowYui/plugins/scheduler.py
@@ -12,7 +12,7 @@ bot = nonebot.get_bot()
 @nonebot.scheduler.scheduled_job('cron', hour='0, 6')
 async def _hour_call_a():
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
-    for i in group:
+    for i in groups:
         try:
             await bot.send_group_msg(group_id=i,
                                      message=f'[日常提醒]已经{now.hour}点啦，へんたいさん记得去商店买经验药剂哦！')
@@ -23,7 +23,7 @@ async def _hour_call_a():
 @nonebot.scheduler.scheduled_job('cron', hour='12, 18')
 async def _hour_call_b():
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
-    for i in group:
+    for i in groups:
         try:
             await bot.send_group_msg(group_id=i,
                                      message=f'[日常提醒]已经{now.hour}点啦，へんたいさん记得去商店买经验药剂哦，别忘了体力任务也可以领取了！')
@@ -34,7 +34,7 @@ async def _hour_call_b():
 @nonebot.scheduler.scheduled_job('cron', hour='5')
 async def _hour_call_c():
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
-    for i in group:
+    for i in groups:
         try:
             await bot.send_group_msg(group_id=i,
                                      message=f'[日常提醒]已经{now.hour}点啦，今天又是没有mana的一天呢。')
@@ -45,7 +45,7 @@ async def _hour_call_c():
 @nonebot.scheduler.scheduled_job('cron', hour='14', minute='45')
 async def _hour_call_d():
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
-    for i in group:
+    for i in groups:
         try:
             await bot.send_group_msg(group_id=i,
                                      message=f'[日常提醒]へんたいさん、准备好背刺了吗？')
